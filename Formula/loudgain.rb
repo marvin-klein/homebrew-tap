420e34da060284b017729cf18340db977d5c78f5
Moonbase59 <moonbase@quantentunnel.de>
2019-08-12 13:50:56 +0200

    loudgain 0.5.3 updated formula

--- Formula/loudgain.rb
+++ Formula/loudgain.rb
@@ -1,5 +1,5 @@
 class Loudgain < Formula
-  desc "ReplayGain 2.0 loudness normalizer based on the EBU R128/ITU BS.1770 standard (-18 LUFS, FLAC, Ogg, MP2, MP3, MP4, M4A, AAC, ALAC, Opus)"
+  desc "ReplayGain 2.0 loudness normalizer based on the EBU R128/ITU BS.1770 standard"
   homepage "https://github.com/Moonbase59/loudgain"
   url "https://github.com/Moonbase59/loudgain/archive/v0.5.3.tar.gz"
   sha256 "2b3258cd48820e2eb579e2357831d40520cb4104bda008d8dfd8188cf2b34db3"
@@ -8,13 +8,14 @@ class Loudgain < Formula
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "ffmpeg"
-  # libtag (=taglib) should be built from HEAD or at least
-  # commit 79bc9ccf8ea5606da2a86e1bfb5439e73a146272
+  # libtag (=taglib) should be built from HEAD
   # because it hasn't seen a new version since 2016.
-  # Before, it had a nasty bug that could clobber .ogg files!
+  # Commit 79bc9ccf8ea5606da2a86e1bfb5439e73a146272 or newer recommended.
+  # Some older versions had a nasty bug that could clobber .ogg files!
   # Use: brew install --HEAD libtag
   # You might need 'brew unlink libtag' before.
   depends_on "libtag"
+  # libebur128 should be version 1.2.4 or newer.
   depends_on "libebur128"
 
   def install
