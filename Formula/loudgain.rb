c14f8d63e10ad716674263108be5e831b8071e85
Moonbase59 <moonbase@quantentunnel.de>
2019-09-01 19:39:54 +0200

    loudgain 0.6.3 (new formula)

--- Formula/loudgain.rb
+++ Formula/loudgain.rb
@@ -1,8 +1,8 @@
 class Loudgain < Formula
   desc "ReplayGain 2.0 loudness normalizer based on the EBU R128/ITU BS.1770 standard"
   homepage "https://github.com/Moonbase59/loudgain"
-  url "https://github.com/Moonbase59/loudgain/archive/v0.6.2.tar.gz"
-  sha256 "5f982a2c1508b5fe07f1993177126069bc5e76abc0aa2f412409f7774b5b6544"
+  url "https://github.com/Moonbase59/loudgain/archive/v0.6.3.tar.gz"
+  sha256 "05d1190852e1deaa52b7570810e9dae6c99fdf654aedf544b33e74b9e584b7db"
   head "https://github.com/Moonbase59/loudgain.git"
 
   depends_on "cmake" => :build
@@ -37,7 +37,7 @@ class Loudgain < Formula
     # The installed folder is not in the path, so use the entire path to any
     # executables being tested: `system "#{bin}/program", "do", "something"`.
     #assert_match /^loudgain\s+#{version}.*/,
-    assert_match /^loudgain\s+0\.6\.2.*/,
+    assert_match /^loudgain\s+0\.6\.3.*/,
       shell_output("#{bin}/loudgain -v").strip
   end
 end
