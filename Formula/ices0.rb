528178b48f116f5180eb1b2eb9234339f685e851
Moonbase59 <moonbase@quantentunnel.de>
2019-09-21 14:55:57 +0200

    ices0 0.4.10 (new formula)

--- Formula/ices0.rb
+++ Formula/ices0.rb
@@ -9,6 +9,7 @@ class Ices0 < Formula
   depends_on "autoconf" => :build
   depends_on "automake" => :build
   depends_on "libtool" => :build
+  depends_on "pkg-config" => :build
   
   # input/output
   depends_on "libshout"
